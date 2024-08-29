1e91955f4c685cb57812cab6bc4074b450f71ef4
AdmiN <AdmiN@ITWS069>
2024-08-29 11:39:51 +0530

    chesa unchuko vaaduko

--- Devops2/src/com/lbrce/Sample.java
+++ Devops2/src/com/lbrce/Sample.java
@@ -16,3 +16,19 @@ public class Sample {
 	}
 
 }
+ class Sample2 {
+	public void multiplication(int a,int b) {
+		System.out.println(a*b);
+	}
+	public void subtract(int a,int b) {
+		System.out.println(a-b);
+	}
+	public static void main(String args[]) {
+		int a=100;
+		int b=20;
+		Sample2 s=new Sample2();
+		s.multiplication(a, b);
+		s.subtract(a, b);
+	}
+
+}
