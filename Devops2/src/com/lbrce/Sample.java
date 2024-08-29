5edd7dec2e05da281c4b1f8dcdac1935534bc909
AdmiN <AdmiN@ITWS070>
2024-08-29 10:46:35 +0530

    commited

---
+++ Devops2/src/com/lbrce/Sample.java
@@ -0,0 +1,18 @@
+package com.lbrce;
+
+public class Sample {
+	public void add(int a,int b) {
+		System.out.println(a+b);
+	}
+	public void subtract(int a,int b) {
+		System.out.println(a-b);
+	}
+	public static void main(String args[]) {
+		int a=10;
+		int b=20;
+		Sample s=new Sample();
+		s.add(a, b);
+		s.subtract(a, b);
+	}
+
+}
